c42953c4a305b1f15b1c4f7938f442735a11e96c
Daiana <dauipoma@gmail.com>
2023-05-02 22:22:52 +0200

    Add router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
+import { Layout } from '../layout/Layout'
 import { Home } from '../pages/Home'
+import { NotFound } from '../pages/NotFound/NotFound'
+import { Recipes } from '../pages/Recipes'
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='*' element={<p>Not Found</p>} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/recipes' element={<Recipes />}>
+          <Route path=':id' element={<Recipes />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Route>
     </Routes>
   )
 }
