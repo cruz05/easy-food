f97244a378b0c6e102fddbef89f218d4325d964b
Daiana <dauipoma@gmail.com>
2023-05-02 22:25:32 +0200

    Change font

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -3,11 +3,17 @@ import styled from 'styled-components'
 export const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  flex-flow: wrap row;
+  justify-content: space-between;
   background-color: #222831;
-  padding: 1rem 0.8rem;
+  padding: 1.5rem 3rem;
   color: white;
   box-sizing: border-box;
+
+  .brand {
+    font-size: 3.4rem;
+    font-weight: 500;
+  }
 
   a {
     text-decoration: none;
@@ -16,19 +22,28 @@ export const StyledHeader = styled.header`
 
   nav {
     display: flex;
-    gap: 0.5rem;
+    gap: 1rem;
+
+    @media (max-width: 500px) {
+      display: none;
+    }
 
     a {
       padding: 0.4rem 1rem;
       border-radius: 0.4rem;
-      font-weight: 600;
+    }
+    .active {
+      // border-bottom: 1px solid #ffd369;
+      color: #ffd369;
     }
   }
   .btn-login {
+    font-weight: 600;
     border: 1px solid #ffd369;
   }
 
   .btn-register {
+    font-weight: 600;
     background-color: #ffd369;
     color: #000;
   }
