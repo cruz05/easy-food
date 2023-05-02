c590480a47542f7eaee6d497c839e6db92e9dba8
Daiana <dauipoma@gmail.com>
2023-05-02 22:25:32 +0200

    Change font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { StyledHeader } from './Header.styles'
 
 export function Header() {
   return (
     <StyledHeader>
-      <img src='' alt='Logo' />
       <Link to='/'>
-        <h1>EasyFood</h1>
+        <p className='brand'>EasyFood</p>
       </Link>
       <nav>
-        <Link className='btn-login' to='/login'>
+        <NavLink
+          className={({ isActive }) => (isActive ? 'active' : undefined)}
+          to='/recipes'>
+          Recipes
+        </NavLink>
+        <NavLink className='btn-login' to='/login'>
           Sign in
-        </Link>
-        <Link className='btn-register' to='/register'>
+        </NavLink>
+        <NavLink className='btn-register' to='/register'>
           Register
-        </Link>
+        </NavLink>
       </nav>
     </StyledHeader>
   )
