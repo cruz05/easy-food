fc7b2720ab7596a25512581dc84ee5d826fc17d9
Daiana <dauipoma@gmail.com>
2023-05-24 23:10:46 +0200

    Refactor recipes page

--- src/hooks/useRecipes.js
+++ src/hooks/useRecipes.js
@@ -3,8 +3,19 @@ import { getRecipes } from '../services/recipes'
 
 export function useRecipes() {
   const [recipes, setRecipes] = useState([])
+  const [titleSearch, setTitleSearch] = useState('')
+  const [filter, setFilter] = useState('all')
+
+  const handleRecipeSearch = search => setTitleSearch(search)
+  const handleFilterSelect = filter => setFilter(filter)
+
   useEffect(() => {
-    getRecipes().then(newRecipes => setRecipes([...newRecipes]))
-  }, [])
-  return { recipes }
+    getRecipes(filter, titleSearch)
+      .then(newRecipes => {
+        newRecipes && setRecipes([...newRecipes])
+      })
+      .catch(error => console.error(error))
+  }, [filter, titleSearch])
+
+  return { recipes, handleRecipeSearch, handleFilterSelect }
 }
