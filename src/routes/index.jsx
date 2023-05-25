75b8ad83335117434f78bc5f6c498bd43a85b553
Daiana <dauipoma@gmail.com>
2023-05-25 22:54:55 +0200

    Add routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,28 @@
 import { Route, Routes } from 'react-router-dom'
-import { Form } from '../components/Form/Form'
 import { Layout } from '../layout/Layout'
-import { Home } from '../pages/Home'
+import { Home } from '../pages/Home/Home'
 import { NotFound } from '../pages/NotFound/NotFound'
-import { Recipes } from '../pages/Recipes'
-import { AuthContextProvider } from '../context/AuthContext'
+import { Recipes } from '../pages/Recipes/Recipes'
+import { Login } from '../pages/Login'
+import { Register } from '../pages/Register'
+import { Account } from '../pages/Account/Account'
+import { ProtectedRoute } from './ProtectedRoute'
 
 export function Router() {
   return (
-    <AuthContextProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/recipes' element={<Recipes />}>
-            <Route path=':id' element={<Recipes />} />
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path='/account/' element={<Account />}></Route>
+          <Route path='/recipes'>
+            <Route index element={<Recipes />} />
           </Route>
-          <Route path='/login' element={<Form />} />
-          <Route path='*' element={<NotFound />} />
         </Route>
-      </Routes>
-    </AuthContextProvider>
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
   )
 }
