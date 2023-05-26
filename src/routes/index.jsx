75fa1944d448b82d0ec6fe4d1488beacf242bc5b
Daiana <dauipoma@gmail.com>
2023-05-26 22:06:24 +0200

    Add routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,8 +5,12 @@ import { NotFound } from '../pages/NotFound/NotFound'
 import { Recipes } from '../pages/Recipes/Recipes'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
+import { RecipeDetails } from '../pages/RecipeDetails/RecipeDetails'
 import { Account } from '../pages/Account/Account'
 import { ProtectedRoute } from './ProtectedRoute'
+import { Profile } from '../components/Profile/Profile'
+import { UserRecipes } from '../components/UserRecipes/UserRecipes'
+import { Collections } from '../components/Collection/Collections'
 
 export function Router() {
   return (
@@ -16,9 +20,14 @@ export function Router() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route element={<ProtectedRoute />}>
-          <Route path='/account/' element={<Account />}></Route>
+          <Route path='/account/' element={<Account />}>
+            <Route index element={<Profile />} />
+            <Route path='recipes' element={<UserRecipes />} />
+            <Route path='collections' element={<Collections />} />
+          </Route>
           <Route path='/recipes'>
             <Route index element={<Recipes />} />
+            <Route path=':id/' element={<RecipeDetails />} />
           </Route>
         </Route>
         <Route path='*' element={<NotFound />} />
